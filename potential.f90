832a72560ee3f9f57389976dada16d72a07fed32
jwmmorley <jwmorley73@gmail.com>
2021-12-11 14:18:07 +0000

    Fixed Issue where applying the same start and end layer resulted in Nan. Also made the order of start and end not matter as well as allow for cropping of well outside of potential bounds

--- potential.f90
+++ potential.f90
@@ -4,19 +4,21 @@ module potential
     public :: potential_add_well
     
 contains
-    subroutine potential_add_well(array, start_ly, end_ly , initial_depth, final_depth)
+    subroutine potential_add_well(array, start_ly, end_ly, initial_depth, final_depth)
     real*8,  intent(inout) :: array(:)
     integer, intent(in)    :: start_ly, end_ly
     real*8,  intent(in)    :: initial_depth, final_depth
     real*8                 :: potential(end_ly - start_ly + 1)
     integer                :: i
     potential = initial_depth
-    do i = 0, end_ly - start_ly 
-        potential(i + 1) = potential(i + 1) + (final_depth - initial_depth) * i / (end_ly - start_ly)
-    end do
+    if (end_ly /= start_ly) then
+        do i = 1, end_ly - start_ly + 1 
+            potential(i) = potential(i) + (final_depth - initial_depth) * (i - 1) / (end_ly - start_ly)
+        end do
+    end if
     do i = 1, size(potential)
         if (start_ly >= 1 .and. end_ly <= size(array)) then 
-            array(start_ly + i -1 ) = array(start_ly + i -1) + potential(i)
+            array(start_ly + i - 1) = array(start_ly + i -1) + potential(i)
         else 
             print*, ' The layer no. inputted is out of bound for potential. '
         end if
